53386041be5aa71170f10a51fe05c46b1c326aa3
Priyanshu Agnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2023-11-24 21:46:20 +0530

    Add files via upload

---
+++ Portfolio-React-main/src/data/data.js
@@ -0,0 +1,14 @@
+
+import p1 from "../assets/projects/p1.png";
+
+export const data = [
+    {
+        id: 1,
+        name: "Full stack Application",
+        image: p4,
+        github: "https://github.com/PriyanshuAgnihotri/test-1",
+    },
+    
+
+
+]
