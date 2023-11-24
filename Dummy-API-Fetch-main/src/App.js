4008b3d361a22cc2cd65f519df6d6b0f5fc0866a
Priyanshu Agnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2023-11-24 21:31:45 +0530

    Add files via upload

---
+++ Dummy-API-Fetch-main/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+// import UserTable from "./components/UserTable";
+import UserTable from "./Components/UserTable";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UserTable /> */}
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
